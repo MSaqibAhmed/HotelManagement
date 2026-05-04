3497d788f7b61aeb8aa9cf84c147ab86d06c4d92
Saqib Ahmed <153153100+MSaqibAhmed@users.noreply.github.com>
2026-05-05 03:45:04 +0500

    Fix Vercel Deployment

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hotelmanagement-staging.up.railway.app/api",
+  baseURL: "https://hotel-management-seven-jet.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
